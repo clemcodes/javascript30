e58aaaa1f3ea6ec77f7ea767b3580df44b301ea5
cl3m3nt1n3 <shukyin.lau@outlook.com>
2020-10-27 19:27:47 +0000

    add css & js

---
+++ 03-updateCSSVariables/script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('input');
+
+
+function handleUpdate(){
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change',handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove',handleUpdate));
