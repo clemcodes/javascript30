d92b35cf5036b3b338942c72aa53795e4bc9bc45
cl3m3nt1n3 <shukyin.lau@outlook.com>
2020-11-03 18:23:03 +0000

    add js

--- 07 - Array Cardio Day2/script.js
+++ 07 - Array Cardio Day2/script.js
@@ -17,13 +17,40 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people.some(person => (new Date()).getFullYear() - person.year >= 19 )
+
+console.log(isAdult);
 // Array.prototype.every() // is everyone 19 or older?
+const allAdults = people.every(person =>  {
+  const currentYear= (new Date()).getFullYear();
+    return currentYear - person.year >= 19;
+})
+
+console.log(allAdults);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment);
+
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
-
  // delete the comment with the ID of 823423
+ const index = comments.findIndex(comment => comment.id === 823423);
+
+ // 2 ways of deleting an item in an array 
+
+  //#1
+ // popular in Redux world
+ // make a new array, slice and spread
+  const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+  ];
+console.table(newComments);
+
+ // splice
+//#2
+ comments.splice(index, 1);
