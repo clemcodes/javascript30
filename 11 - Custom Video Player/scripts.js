c5e0e7d829de844031f7965b89d3009bd7c9a8ed
cl3m3nt1n3 <shukyin.lau@outlook.com>
2020-11-24 13:16:43 +0000

    toggle js

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,37 @@
+// Get Our Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+console.dir(video);
+// Build out functions
+function togglePlay(){
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+//   if(video.paused){
+//     video.play();
+//   } else {
+//     video.pause();
+//   }
+}
+
+function updateButton(){
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip(){
+  console.log('skipping');
+}
+
+// Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
